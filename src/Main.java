e77af8031879a0ae557d4b33de613fe9f4b33e0a
dorzho_tsyrenov <dorzhogun@yandex.ru>
2022-09-20 21:34:26 +0300

    dev

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,20 @@
 import java.io.File;
+import java.util.HashMap;
 import java.util.concurrent.ForkJoinPool;
+
+import static java.lang.Long.*;
 
 public class Main
 {
+    private static final char[] sizeMultipliers = {'B', 'K', 'M', 'G', 'T'};
+
     public static void main(String[] args)
     {
+        System.out.println(getHumanReadableSize(240640));
+        System.exit(0);
 
+        System.out.println(getSizeFromHumanReadable("235K"));
+        System.exit(0);
 
         String folderPath = "C:/Users/Артур/Desktop/Folder";
         File file = new File(folderPath);
@@ -18,22 +27,62 @@ public class Main
         long size = pool.invoke(calculator);
         System.out.println(size);
 
+
+
         long duration = (System.currentTimeMillis() - start);
         System.out.println(duration + " ms");
     }
 
-    public static long getFolderSize(File folder)
+//    public static long getFolderSize(File folder)
+//    {
+//        if(folder.isFile()) {
+//            return folder.length();
+//        }
+//        long sum = 0;
+//        File[] files = folder.listFiles();
+//        if (files != null) {
+//            for(File file : files) {
+//                sum += getFolderSize(file);
+//            }
+//        }
+//        return sum;
+//    }
+    //TODO: 24B, 234Kb, 36Mb, 34Gb, 42Tb
+    // 24B, 234K, 36M, 34G, 42T
+    // 235K => 240640
+    public static String getHumanReadableSize(long size)
     {
-        if(folder.isFile()) {
-            return folder.length();
-        }
-        long sum = 0;
-        File[] files = folder.listFiles();
-        if (files != null) {
-            for(File file : files) {
-                sum += getFolderSize(file);
+        for (int i = 0; i < sizeMultipliers.length; i++)
+        {
+            double value = size / Math.pow(1024, i);
+            if(value < 1024) {
+                return Math.round(value) + "" + sizeMultipliers[i] +
+                        (i > 0 ? "b" : "");
             }
         }
-        return sum;
+        return "Very big!";
+    }
+
+    public static long getSizeFromHumanReadable(String size)
+    {
+        HashMap<Character, Integer> char2multiplier = getMultipliers();
+        char sizeFactor = size.replaceAll("[0-9\\s+]+", "").charAt(0);
+        int multiplier = char2multiplier.get(sizeFactor);
+        long length;
+        length = multiplier * parseLong(size.replaceAll("[^0-9]", ""));
+        return length;
+    }
+
+    private static HashMap<Character, Integer> getMultipliers()
+    {
+
+        HashMap<Character, Integer> char2Multiplier = new HashMap<>();
+        for (int i = 0; i < sizeMultipliers.length; i++) {
+            char2Multiplier.put(
+                    sizeMultipliers[i],
+                    (int) Math.pow(1024, i)
+            );
+        }
+        return char2Multiplier;
     }
 }
